f4b37e2d8d30437730f5740232a31118c4f6aa4f
CookieCodesAI <kdhriti15@gmail.com>
2026-01-17 14:44:11 -0500

    added shared button panel and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
+import ProfilePic from "./components/ProfilePic.jsx"
+import Scrollbar from './components/Scrollbar.jsx'
+import ButtonPanel from './components/ButtonPanel.jsx'
+import Call from "./pages/Call.jsx"
+import Text from "./pages/Text.jsx"
+import LiveStream from "./pages/LiveStream.jsx"
+import Location from "./pages/Location.jsx"
+import {Routes, Route} from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
+  return ( 
+    <Routes>
+      <Route path="/" element={
+        <div className='container'>
+          <ProfilePic/>
+          <Scrollbar/>
+          <ButtonPanel/>
+        </div> 
+      }/>
+      <Route path = '/call' element = {<Call/>} />
+      {<Route path = '/text' element = {<Text/>} />}
+      <Route path = '/livestream' element = {<LiveStream/>} />
+      {<Route path = '/location' element = {<Location/>} />}
+    </Routes>
+  ) } 
 export default App
