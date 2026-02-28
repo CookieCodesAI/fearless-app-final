3f3764ccd4e0b1edd32eade831e977cb74666cc7
CookieCodesAI <kdhriti15@gmail.com>
2026-02-28 14:55:20 -0500

    added sanvi's files from previous repository

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,19 @@ import Call from "./pages/Call.jsx"
 import Text from "./pages/Text.jsx"
 import {Routes, Route} from 'react-router-dom'
 import LiveStream from "./pages/LiveStream.jsx"
-
+import Location from "./pages/Location.jsx" 
+import L from "leaflet"; 
+import "leaflet/dist/leaflet.css"; 
+import icon from "leaflet/dist/images/marker-icon.png"; 
+import icon2x from "leaflet/dist/images/marker-icon-2x.png"; 
+import shadow from "leaflet/dist/images/marker-shadow.png";
 function App() { 
+  delete L.Icon.Default.prototype._getIconUrl; 
+  L.Icon.Default.mergeOptions({ 
+    iconRetinaUrl: icon2x, 
+    iconUrl: icon, 
+    shadowUrl: shadow,
+  })
   return ( 
     <Routes>
       <Route path="/" element={
